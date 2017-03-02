e63f238896575058bb2fce4d1d78e06a0d280895
Adamo Morone <adamo.morone@gmail.com>
2017-03-02 07:03:16 -0300

    Removed Python server. Unecessary.

--- server/c-server/systools.h
+++
@@ -1,5 +0,0 @@
-void SYS_LOG_INFO(char *message);
-void SYS_LOG_DEBUG(char *message);
-void SYS_LOG_ERROR(char *message);
-void write_to_file(char *message);
-void outputLog(char *logType, char *message);
